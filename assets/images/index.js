0998705b61a610539a03da1d610f071aa6be5512
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-06-18 12:49:16 +0100

    chore: added svg icons and images file

---
+++ assets/images/index.js
@@ -0,0 +1,7 @@
+export const Burger = require("./burger.png");
+export const Cheese = require("./cheese.png");
+export const Rice = require("./rice.png");
+export const Turkey = require("./turkey.png");
+export const Soup = require("./soup.png");
+export const Pizza = require("./pizza.png");
+export const Modal = require("./modal.png");
